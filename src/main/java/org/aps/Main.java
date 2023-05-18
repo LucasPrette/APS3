64f6a30f699dff9c65cca725b803ae40ee1bad51
Matheus Ferreira <mattferreira.dev@protonmail.com>
2023-05-18 09:53:43 -0300

    refactor(menu): delete class

--- src/main/java/org/aps/Main.java
+++ src/main/java/org/aps/Main.java
@@ -1,7 +1,11 @@
 package org.aps;
 
+import org.aps.repositories.EndangeredSpeciesRepository;
+
 public class Main {
     public static void main(String[] args) {
-        new Menu().execute();
+        EndangeredSpeciesRepository endangeredSpeciesRepository = new EndangeredSpeciesRepository();
+
+//            System.out.println(endangeredSpeciesRepository.findAllByType("LpS9nflQ9RBXPDcUhuMQ").get(0).getGroup().getName());
     }
 }
